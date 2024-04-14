4376b088a1f7e6946c84118c64f220d8eda3156f
DasNilima <nilima_sust@yahoo.com>
2024-04-13 21:22:57 -0700

    add client side functionality

---
+++ client/src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import { Box, Typography, Tab, Tabs, useMediaQuery } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import Item from "../../components/item";
+import { setItems } from "../../state";
+
+const ShoppingList = () => {
+    const dispatch = useDispatch();
+    const [value, setValue] = useState("all");
+    const items = useSelector((state) => state.cart.items);
+    const breakPoint = useMediaQuery("(min-width:600px)");
+    console.log("item", items);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    async function getItems() {
+        const items = await fetch(
+            "http://localhost:1337/api/items?populate=image",
+            { method: "GET" }
+        );
+        const itemsJson = await items.json();
+        dispatch(setItems(itemsJson.data));
+    }
+    useEffect(() => {
+        getItems();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+
+const topRatedItems = items.filter(
+    (item) => item.attributes.category === "NewReleases"
+);
+const newArrivalsItems = items.filter(
+    (item) => item.attributes.category === "NewArrivals"
+);
+const bestSellersItems = items.filter(
+    (item) => item.attributes.category === "BestSellers"
+);
+    return (
+        <Box width="80%" margin="80px auto">
+        <Typography variant="h3" textAlign="center">
+            Our Featured <b>Products</b>
+        </Typography>
+        <Tabs
+            textColor="primary"
+            indicatorColor="primary"
+            value={value}
+            onChange={handleChange}
+            centered
+            TabIndicatorProps={{ sx: { display: breakPoint ? "block" : "none" } }}
+        sx={{
+            m: "25px",
+            "& .MuiTabs-flexContainer": {
+            flexWrap: "wrap",
+            },
+        }}
+        >
+            <Tab label="ALL" value="all" />
+            <Tab label="NEW RELEASES" value="NewReleases" />
+            <Tab label="NER ARRIVALS" value="NewArrivals" />
+            <Tab label="BEST SELLERS" value="BestSellers" />
+        </Tabs>
+        <Box
+            margin="0 auto"
+            display="grid"
+            gridTemplateColumns="repeat(auto-fill, 300px)"
+            justifyContent="space-around"
+            rowGap="20px"
+            columnGap="1.33%"
+        >
+        {value === "all" &&
+            items.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+            ))}
+        {value === "NewReleases" &&
+            newArrivalsItems.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+            ))}
+        {value === "NewArrivals" &&
+            bestSellersItems.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+            ))}
+        {value === "BestSellers" &&
+            topRatedItems.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+            ))}
+        </Box>
+    </Box>
+    );
+};
+
+
+export default ShoppingList;
